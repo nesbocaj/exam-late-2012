f55f06126064ada27e46695c55d0166bc451603c
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-17 13:37:01 +0100

    Basic UI Started
    testing to see if dis shit works

--- front-end/MainForm.Designer.cs
+++ front-end/MainForm.Designer.cs
@@ -28,20 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redigerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hjælpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deltagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kursusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.klasseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medarbejderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lærerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kontorassistentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fakturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.åbnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hjælpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(887, 494);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(879, 468);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filerToolStripMenuItem,
+            this.redigerToolStripMenuItem,
+            this.hjælpToolStripMenuItem,
+            this.hjælpToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filerToolStripMenuItem
+            // 
+            this.filerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nyToolStripMenuItem,
+            this.åbnToolStripMenuItem});
+            this.filerToolStripMenuItem.Name = "filerToolStripMenuItem";
+            this.filerToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.filerToolStripMenuItem.Text = "Filer";
+            // 
+            // redigerToolStripMenuItem
+            // 
+            this.redigerToolStripMenuItem.Name = "redigerToolStripMenuItem";
+            this.redigerToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.redigerToolStripMenuItem.Text = "Rediger";
+            // 
+            // hjælpToolStripMenuItem
+            // 
+            this.hjælpToolStripMenuItem.Name = "hjælpToolStripMenuItem";
+            this.hjælpToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.hjælpToolStripMenuItem.Text = "Vis";
+            // 
+            // nyToolStripMenuItem
+            // 
+            this.nyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deltagerToolStripMenuItem,
+            this.kursusToolStripMenuItem,
+            this.klasseToolStripMenuItem,
+            this.medarbejderToolStripMenuItem,
+            this.fakturaToolStripMenuItem});
+            this.nyToolStripMenuItem.Name = "nyToolStripMenuItem";
+            this.nyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nyToolStripMenuItem.Text = "Ny";
+            // 
+            // deltagerToolStripMenuItem
+            // 
+            this.deltagerToolStripMenuItem.Name = "deltagerToolStripMenuItem";
+            this.deltagerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deltagerToolStripMenuItem.Text = "Deltager";
+            // 
+            // kursusToolStripMenuItem
+            // 
+            this.kursusToolStripMenuItem.Name = "kursusToolStripMenuItem";
+            this.kursusToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.kursusToolStripMenuItem.Text = "Kursus";
+            // 
+            // klasseToolStripMenuItem
+            // 
+            this.klasseToolStripMenuItem.Name = "klasseToolStripMenuItem";
+            this.klasseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.klasseToolStripMenuItem.Text = "Klasse";
+            // 
+            // medarbejderToolStripMenuItem
+            // 
+            this.medarbejderToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lærerToolStripMenuItem,
+            this.kontorassistentToolStripMenuItem});
+            this.medarbejderToolStripMenuItem.Name = "medarbejderToolStripMenuItem";
+            this.medarbejderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.medarbejderToolStripMenuItem.Text = "Medarbejder";
+            // 
+            // lærerToolStripMenuItem
+            // 
+            this.lærerToolStripMenuItem.Name = "lærerToolStripMenuItem";
+            this.lærerToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.lærerToolStripMenuItem.Text = "Lærer";
+            // 
+            // kontorassistentToolStripMenuItem
+            // 
+            this.kontorassistentToolStripMenuItem.Name = "kontorassistentToolStripMenuItem";
+            this.kontorassistentToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.kontorassistentToolStripMenuItem.Text = "Kontorassistent";
+            // 
+            // fakturaToolStripMenuItem
+            // 
+            this.fakturaToolStripMenuItem.Name = "fakturaToolStripMenuItem";
+            this.fakturaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fakturaToolStripMenuItem.Text = "Faktura";
+            // 
+            // åbnToolStripMenuItem
+            // 
+            this.åbnToolStripMenuItem.Name = "åbnToolStripMenuItem";
+            this.åbnToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.åbnToolStripMenuItem.Text = "Afslut";
+            // 
+            // hjælpToolStripMenuItem1
+            // 
+            this.hjælpToolStripMenuItem1.Name = "hjælpToolStripMenuItem1";
+            this.hjælpToolStripMenuItem1.Size = new System.Drawing.Size(51, 20);
+            this.hjælpToolStripMenuItem1.Text = "Hjælp";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(887, 518);
-            this.Name = "Form1";
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deltagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kursusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem klasseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medarbejderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lærerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kontorassistentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fakturaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem åbnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redigerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hjælpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hjælpToolStripMenuItem1;
     }
 }
 
