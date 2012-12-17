8b38a6ab78d3162d3d3c5f05d8e28da92b3f27f5
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-17 14:19:32 +0100

    Added BaseTabControl

---
+++ front-end/BaseTabControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam_late_2012
+{
+    public partial class BaseTabControl : UserControl
+    {
+        public BaseTabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
