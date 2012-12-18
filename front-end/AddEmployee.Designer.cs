200f7f8ae1d67c1d5df97a1a11e5250d920e3dd8
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-18 13:52:28 +0100

    AddEmployee Window nearly done
    all that's missing is naming all the textboxes

--- front-end/AddEmployee.Designer.cs
+++ front-end/AddEmployee.Designer.cs
@@ -29,12 +29,22 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.OKButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
-            this.baseAdressControl1 = new exam_late_2012.BaseAdressControl();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.basePersonControl1 = new exam_late_2012.BasePersonControl();
+            this.baseAdressControl1 = new exam_late_2012.BaseAdressControl();
             this.baseConstactControl1 = new exam_late_2012.BaseConstactControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,20 +52,10 @@
             this.panel1.Controls.Add(this.CancelButton);
             this.panel1.Controls.Add(this.OKButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 290);
+            this.panel1.Location = new System.Drawing.Point(0, 265);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(445, 30);
             this.panel1.TabIndex = 0;
-            // 
-            // OKButton
-            // 
-            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.OKButton.Location = new System.Drawing.Point(358, 3);
-            this.OKButton.Name = "OKButton";
-            this.OKButton.Size = new System.Drawing.Size(75, 23);
-            this.OKButton.TabIndex = 0;
-            this.OKButton.Text = "OK";
-            this.OKButton.UseVisualStyleBackColor = true;
             // 
             // CancelButton
             // 
@@ -67,39 +67,139 @@
             this.CancelButton.Text = "Annullér";
             this.CancelButton.UseVisualStyleBackColor = true;
             // 
-            // baseAdressControl1
+            // OKButton
             // 
-            this.baseAdressControl1.Location = new System.Drawing.Point(12, 124);
-            this.baseAdressControl1.Name = "baseAdressControl1";
-            this.baseAdressControl1.Size = new System.Drawing.Size(212, 155);
-            this.baseAdressControl1.TabIndex = 2;
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKButton.Location = new System.Drawing.Point(358, 3);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 0;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.basePersonControl1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.baseAdressControl1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.baseConstactControl1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.62264F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60.37736F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(445, 265);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // basePersonControl1
             // 
-            this.basePersonControl1.Location = new System.Drawing.Point(12, 8);
+            this.basePersonControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.basePersonControl1.Location = new System.Drawing.Point(3, 3);
             this.basePersonControl1.Name = "basePersonControl1";
-            this.basePersonControl1.Size = new System.Drawing.Size(212, 110);
-            this.basePersonControl1.TabIndex = 1;
+            this.basePersonControl1.Size = new System.Drawing.Size(216, 99);
+            this.basePersonControl1.TabIndex = 0;
+            // 
+            // baseAdressControl1
+            // 
+            this.baseAdressControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.baseAdressControl1.Location = new System.Drawing.Point(3, 108);
+            this.baseAdressControl1.Name = "baseAdressControl1";
+            this.baseAdressControl1.Size = new System.Drawing.Size(216, 154);
+            this.baseAdressControl1.TabIndex = 1;
             // 
             // baseConstactControl1
             // 
-            this.baseConstactControl1.Location = new System.Drawing.Point(230, 8);
+            this.baseConstactControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.baseConstactControl1.Location = new System.Drawing.Point(225, 3);
             this.baseConstactControl1.Name = "baseConstactControl1";
-            this.baseConstactControl1.Size = new System.Drawing.Size(203, 108);
-            this.baseConstactControl1.TabIndex = 3;
+            this.baseConstactControl1.Size = new System.Drawing.Size(217, 99);
+            this.baseConstactControl1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(225, 108);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 154);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Medarbejder Information:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(111, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(111, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rank:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Initialer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Adgangskode:";
             // 
             // AddEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 320);
-            this.Controls.Add(this.baseConstactControl1);
-            this.Controls.Add(this.baseAdressControl1);
-            this.Controls.Add(this.basePersonControl1);
+            this.ClientSize = new System.Drawing.Size(445, 295);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.panel1);
             this.Name = "AddEmployee";
             this.Text = "AddEmployee";
             this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -109,8 +209,16 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button CancelButton;
         private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private BasePersonControl basePersonControl1;
         private BaseAdressControl baseAdressControl1;
         private BaseConstactControl baseConstactControl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
