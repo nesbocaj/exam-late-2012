d9b4f7ed645f95e0383795f550cba85f873a7641
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-19 15:11:56 +0100

    gets and set added to class model

--- back-end/Class.cs
+++ back-end/Class.cs
@@ -13,27 +13,32 @@ namespace back_end
 
         public int ID
         {
-
+            set { _classId = value; }
+            get { return _classId; }
         }
 
         public int RoomID
         {
-
+            set { _classRoomId = value; }
+            get { return _classRoomId; }
         }
 
         public int PersonID
         {
-
+            set { _personId = value; }
+            get { return _personId; }
         }
 
         public int CourseID
         {
-
+            set { _courseId = value; }
+            get { return _courseId; }
         }
 
         public string CourseName
         {
-
+            set { _courseName = value; }
+            get { return _courseName; }
         }
 
         public void Upadte()
