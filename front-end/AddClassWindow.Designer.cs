4bc45c45c5a0818d7b62d46586365056b97fdc58
Dennis Kong Kei Liu <de.liu@ofir.dk>
2012-12-19 12:55:23 +0100

    Update AddClassWindow

--- front-end/AddClassWindow.Designer.cs
+++ front-end/AddClassWindow.Designer.cs
@@ -33,17 +33,30 @@
             this.OKButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.ClassNameText = new System.Windows.Forms.TextBox();
-            this.ClassRoomText = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.CourseText = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ClassList = new System.Windows.Forms.ListBox();
+            this.UnassignedList = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.CourseCombo = new System.Windows.Forms.ComboBox();
+            this.ClassRoomCombo = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -53,13 +66,13 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 317);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(427, 30);
+            this.panel1.Size = new System.Drawing.Size(504, 30);
             this.panel1.TabIndex = 9;
             // 
             // CancelButton
             // 
             this.CancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.CancelButton.Location = new System.Drawing.Point(259, 3);
+            this.CancelButton.Location = new System.Drawing.Point(336, 3);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(75, 23);
             this.CancelButton.TabIndex = 1;
@@ -69,7 +82,7 @@
             // OKButton
             // 
             this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.OKButton.Location = new System.Drawing.Point(340, 3);
+            this.OKButton.Location = new System.Drawing.Point(417, 3);
             this.OKButton.Name = "OKButton";
             this.OKButton.Size = new System.Drawing.Size(75, 23);
             this.OKButton.TabIndex = 0;
@@ -89,20 +102,13 @@
             // 
             this.ClassNameText.Location = new System.Drawing.Point(100, 19);
             this.ClassNameText.Name = "ClassNameText";
-            this.ClassNameText.Size = new System.Drawing.Size(100, 20);
+            this.ClassNameText.Size = new System.Drawing.Size(121, 20);
             this.ClassNameText.TabIndex = 11;
-            // 
-            // ClassRoomText
-            // 
-            this.ClassRoomText.Location = new System.Drawing.Point(100, 45);
-            this.ClassRoomText.Name = "ClassRoomText";
-            this.ClassRoomText.Size = new System.Drawing.Size(100, 20);
-            this.ClassRoomText.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Location = new System.Drawing.Point(10, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 13;
@@ -111,32 +117,36 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.02381F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.97619F));
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(427, 317);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(504, 317);
             this.tableLayoutPanel1.TabIndex = 14;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.ClassRoomCombo);
+            this.groupBox1.Controls.Add(this.CourseCombo);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.ClassNameText);
-            this.groupBox1.Controls.Add(this.CourseText);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.ClassRoomText);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(207, 152);
+            this.groupBox1.Size = new System.Drawing.Size(231, 152);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Hold Detaljer";
@@ -144,43 +154,152 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 104);
+            this.label4.Location = new System.Drawing.Point(10, 131);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.Size = new System.Drawing.Size(82, 13);
             this.label4.TabIndex = 16;
-            this.label4.Text = "Afslutningsdato";
+            this.label4.Text = "Afslutningsdato:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 74);
+            this.label3.Location = new System.Drawing.Point(10, 48);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 17;
             this.label3.Text = "Kursus:";
             // 
-            // CourseText
-            // 
-            this.CourseText.Location = new System.Drawing.Point(100, 71);
-            this.CourseText.Name = "CourseText";
-            this.CourseText.Size = new System.Drawing.Size(100, 20);
-            this.CourseText.TabIndex = 16;
-            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.listBox1);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(3, 161);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(207, 153);
+            this.groupBox2.Size = new System.Drawing.Size(231, 153);
             this.groupBox2.TabIndex = 16;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Underviser";
+            this.groupBox2.Text = "Undervisertildeling";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(212, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.UnassignedList);
+            this.groupBox3.Controls.Add(this.ClassList);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(240, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.tableLayoutPanel1.SetRowSpan(this.groupBox3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(261, 311);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kursisttildeling";
+            // 
+            // ClassList
+            // 
+            this.ClassList.FormattingEnabled = true;
+            this.ClassList.Location = new System.Drawing.Point(155, 41);
+            this.ClassList.Name = "ClassList";
+            this.ClassList.Size = new System.Drawing.Size(100, 264);
+            this.ClassList.TabIndex = 0;
+            // 
+            // UnassignedList
+            // 
+            this.UnassignedList.FormattingEnabled = true;
+            this.UnassignedList.Location = new System.Drawing.Point(6, 41);
+            this.UnassignedList.Name = "UnassignedList";
+            this.UnassignedList.Size = new System.Drawing.Size(100, 264);
+            this.UnassignedList.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(152, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Holdliste";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Utildelte Kursister";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Startsdato:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(100, 99);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(122, 20);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(101, 125);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(122, 20);
+            this.dateTimePicker2.TabIndex = 21;
+            // 
+            // CourseCombo
+            // 
+            this.CourseCombo.FormattingEnabled = true;
+            this.CourseCombo.Location = new System.Drawing.Point(100, 45);
+            this.CourseCombo.Name = "CourseCombo";
+            this.CourseCombo.Size = new System.Drawing.Size(121, 21);
+            this.CourseCombo.TabIndex = 22;
+            // 
+            // ClassRoomCombo
+            // 
+            this.ClassRoomCombo.FormattingEnabled = true;
+            this.ClassRoomCombo.Location = new System.Drawing.Point(101, 72);
+            this.ClassRoomCombo.Name = "ClassRoomCombo";
+            this.ClassRoomCombo.Size = new System.Drawing.Size(121, 21);
+            this.ClassRoomCombo.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Undervisernavn:";
             // 
             // AddClassWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 347);
+            this.ClientSize = new System.Drawing.Size(504, 347);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.panel1);
             this.Name = "AddClassWindow";
@@ -189,6 +308,10 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -200,13 +323,24 @@
         private System.Windows.Forms.Button OKButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox ClassNameText;
-        private System.Windows.Forms.TextBox ClassRoomText;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox CourseText;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox UnassignedList;
+        private System.Windows.Forms.ListBox ClassList;
+        private System.Windows.Forms.ComboBox ClassRoomCombo;
+        private System.Windows.Forms.ComboBox CourseCombo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
