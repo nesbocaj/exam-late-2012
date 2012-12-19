dcdfafb4add67efc550675f387bf89121a4e815c
Josef Itani <youssef_itani@hotmail.com>
2012-12-19 13:13:07 +0000

    whitespace commitment

---
+++ back-end/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    class Employee : IModel
+    {
+
+        public Employee emp;
+
+        
+        
+        
+        
+
+        
+        
+    
+    
+    
+    
+    }
+}
