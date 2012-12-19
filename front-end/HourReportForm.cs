85fc505468fd3a8fd11f31c29ac6a0c76ae0cdda
Dennis Kong Kei Liu <de.liu@ofir.dk>
2012-12-19 14:12:34 +0100

    HourReportForm Added

---
+++ front-end/HourReportForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam_late_2012
+{
+    public partial class HourReportForm : BaseAddForm
+    {
+        public HourReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HourReportForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
