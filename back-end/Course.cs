92c1a7df7934cf176e126fdd8e20c6f13665e662
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-19 14:09:38 +0100

    changes to ze course model

--- back-end/Course.cs
+++ back-end/Course.cs
@@ -8,7 +8,9 @@ namespace back_end
 {
     public class Course : IModel
     {
-        private int _id;
+        private int _courseId, _courseTypeId, _diffiulty;
+        private string _courseName;
+        private double _price;
 
         public Course()
         {
@@ -17,8 +19,32 @@ namespace back_end
 
         public int ID
         {
-            set { _id = value; }
-            get { return _id;}
+            set { _courseId = value; }
+            get { return _courseId; }
+        }
+
+        public int TypeID
+        {
+            set { _courseTypeId = value; }
+            get { return _courseTypeId; }
+        }
+
+        public int Difficulty
+        {
+            set { _diffiulty = value; }
+            get { return _diffiulty; }
+        }
+
+        public String CourseName
+        {
+            set { _courseName = value; }
+            get { return _courseName; }
+        }
+
+        public double Price
+        {
+            set { _price = value; }
+            get { return _price; }
         }
 
         public void Update()
