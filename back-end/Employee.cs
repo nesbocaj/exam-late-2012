0ff108cc683af1c4e6e32cc2c2f49cdd39bb9900
Josef Itani <youssef_itani@hotmail.com>
2012-12-19 14:08:23 +0000

    Get Employee attributes from IModel class which calls Get Methods from all our objects

--- back-end/Employee.cs
+++ back-end/Employee.cs
@@ -8,19 +8,50 @@ namespace back_end
 {
     class Employee : IModel
     {
+        public int Id;
+        public int person_Id;
+        public int positions_Id;
+        public int salary_rate;
+        public int working_hours;
+        public string accessword;
 
-        public Employee emp;
+        public Employee(int ID, int person_ID, int positions_ID, int salary_Rate, int working_Hours, string accesWord)
+        {
+            Id = ID;
+            person_Id = person_ID;
+            salary_rate = salary_Rate;
+            working_hours = working_Hours;
+            accessword = accesWord;
+        }
 
-        
-        
-        
-        
+        public int GetEmployeeId(int Id)
+        {
+            return Id;
+        }
 
-        
-        
-    
-    
-    
-    
+        public int GetEmployeePerson_Id(int person_Id)
+        {
+            return person_Id;
+        }
+
+        public int GetEmployeePositions_Id(int positions_Id)
+        {
+            return positions_Id;
+        }
+
+        public int GetEmployeeSalary_rate(int salary_rate)
+        {
+            return salary_rate;
+        }
+
+        public int GetEmployeeWorking_Hours(int working_hours)
+        {
+            return working_hours;
+        }
+
+        public string GetEmployeeaccessWord(string accessword)
+        {
+            return accessword;
+        }   
     }
 }
