85304bc07ff137b33cfa3a1d9b2975937dad8434
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-19 13:22:59 +0100

    first draft of the Course Model added

---
+++ back-end/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    public class Course : IModel
+    {
+        public int ID
+        {
+            get { return 0;}
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
