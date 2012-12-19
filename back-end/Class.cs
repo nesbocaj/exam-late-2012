d82c941d141438771d84249ff4c8c30ec1723b10
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-19 15:08:41 +0100

    Class Model Added
    sorta

---
+++ back-end/Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    public class Class : IModel
+    {
+        private int _classId, _classRoomId, _personId, _courseId;
+        private string _courseName;
+
+        public int ID
+        {
+
+        }
+
+        public int RoomID
+        {
+
+        }
+
+        public int PersonID
+        {
+
+        }
+
+        public int CourseID
+        {
+
+        }
+
+        public string CourseName
+        {
+
+        }
+
+        public void Upadte()
+        {
+
+        }
+    }
+}
