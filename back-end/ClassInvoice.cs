81f62a33921201570cb7fea69968e4c1b56f36b6
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-19 22:03:52 +0100

    Invoice classes added
    waiting for invoice lines

---
+++ back-end/ClassInvoice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    public class ClassInvoice : IModel
+    {
+        private int _id, _classId;
+        private double _total, _paid;
+        //private Dictionary<int, ClassInvoiceLine> invoiceLine;
+
+        public ClassInvoice()
+        {
+
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int ClassID
+        {
+            get { return _classId; }
+            set { _classId = value; }
+        }
+
+        public double Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        public double Paid
+        {
+            get { return _paid; }
+            set { _paid = value; }
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ back-end/ParticipantInvoice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    public class ParticipantInvoice : IModel
+    {
+        private int _id, _personId;
+        private double _total, _paid;
+        //private Dictionary<int, ParticipantInvoiceLine> invoiceLine;
+
+        public ParticipantInvoice()
+        {
+
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int ParticipantID
+        {
+            get { return _personId; }
+            set { _personId = value; }
+        }
+
+        public double Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        public double Paid
+        {
+            get { return _paid; }
+            set { _paid = value; }
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
