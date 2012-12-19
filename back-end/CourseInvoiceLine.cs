1a90879ba953a0525e54180d4bbdc56717d07f12
Josef Itani <youssef_itani@hotmail.com>
2012-12-19 22:41:08 +0000

    CreateCourseInvoice

---
+++ back-end/CourseInvoiceLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+    class CourseInvoiceLine
+    {
+        private int _CourseInvoiceId; private int _PersonId;
+        private double _subtotal;
+
+        public CourseInvoiceLine()
+        {
+
+        }
+
+        public int CourseInvoiceId
+        {
+            set { _CourseInvoiceId = value; }
+            get { return _CourseInvoiceId; }
+        }
+
+        public int PersonId
+        {
+            set { _PersonId = value; }
+            get { return _PersonId; }
+        }
+
+        public double Subtotal
+        {
+            set { _subtotal = value; }
+            get { return _subtotal; }
+        }
+    }
+}
