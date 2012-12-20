5f720562ff0623f9173a1cf64c1ecf2e88b66194
Nikolaj Lepka <slench102+git@gmail.com>
2012-12-20 14:29:50 +0100

    Tutor Class added

---
+++ back-end/Models/Tutor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end.Models
+{
+    public class Tutor : Employee
+    {
+        private int _numberOfClasses, _experience;
+
+        public int NumberOfClasses
+        {
+            get { return _numberOfClasses; }
+            set { _numberOfClasses = value; }
+        }
+
+        public int Experience
+        {
+            get { return _experience; }
+            set { _experience = value; }
+            }
+    }
+}
